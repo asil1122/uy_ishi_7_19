640e2bdbd8b6ef0e4e7afcf842768616b2c2f135
asil1122 <asilbekabdurashidov000gmail.com>
2024-12-20 11:59:44 +0500

    finish

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'storage.kun.uz',
+        port: '', 
+      }
+    ]
+    
+  },
 };
 
 export default nextConfig;
+
+// https://storage.kun.uz/
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,14 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      <h1>Hello World</h1>
+      <Image 
+      src={'https://storage.kun.uz/source/10/C8LegajuTHiCxALYvB8IEAePl0tVowlJ.jpg'} 
+      quality={1}
+      alt="img" 
+      width={400} 
+      height={400}/>
     </div>
   );
 }
